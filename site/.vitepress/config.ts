c423c9d3093f77e7fee7e98a5e82bc1807ce838a
Scott Walker <scott@swlt.ru>
2026-04-04 16:39:37 +0300

    Add brand logo to navbar and full logo to landing page

--- site/.vitepress/config.ts
+++ site/.vitepress/config.ts
@@ -12,7 +12,8 @@ export default defineConfig({
     ['link', { href: 'https://fonts.googleapis.com/css2?family=Alegreya+Sans+SC:wght@400;500;700;800&family=Inter:wght@400;500;600;700&display=swap', rel: 'stylesheet' }],
   ],
   themeConfig: {
-    logo: undefined,
+    logo: '/logo.png',
+    siteTitle: 'Kraube API',
     nav: [
       { text: 'Guide', link: '/guide/getting-started' },
       { text: 'Reference', link: '/reference/api' },
